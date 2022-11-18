a4dcd2d8e0dd777b503ab2a7b91f2e6ae51e3586
Jairo Acosta <jairoacosta_@hotmail.com>
2022-11-17 23:19:35 -0500

    ejercicio de objetos, array y fecha

---
+++ ObjetosYFecha.js
@@ -0,0 +1,14 @@
+
+let nombre='Jairo';
+let edad=41;
+let desarrollador=true;
+let fechaNacimiento=new Date('26 august 1981');
+let datosPersonales = [nombre,edad,desarrollador,fechaNacimiento];
+
+
+const libro={
+    titulo:'El gran libro de HTML5,CSS3 Y JAVASCRIPT',
+    autor:'J.D Gauchat',
+    fecha:'2017',
+    url:'www.marcombo.com'
+}
