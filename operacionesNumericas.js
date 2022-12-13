156cd1b918bac379f3c28faaea2a889b6e78bdc3
Jairo Acosta <jairoacostaq@gmail.com>
2022-12-12 21:52:04 -0500

    operaciones numéricas

---
+++ operacionesNumericas.js
@@ -0,0 +1,14 @@
+let altura_cm =175;
+let altura_mts=1.75;
+let peso_kg=68.4;
+let altura_arriba= Math.ceil(altura_mts);
+
+    console.log(altura_arriba);
+
+let peso_abajo= Math.floor(peso_kg);
+
+    console.log(peso_abajo);
+
+let maximo_comparado= Number.MAX_VALUE===Number.MAX_VALUE+1;
+
+    console.log(maximo_comparado);
