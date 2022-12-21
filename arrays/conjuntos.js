41fcf02b77b982cf9d6b258614080f861e18550c
Jairo Acosta <jairoacostaq@gmail.com>
2022-12-20 22:55:11 -0500

    conjuntos usando la función Set

---
+++ arrays/conjuntos.js
@@ -0,0 +1,8 @@
+let setFamilia = new Set(['Maritza','Ramón','Tia tita','Jaimar','Ramonsito','Jonathan','Corina','Jesus Adrian']);
+console.log(setFamilia);
+
+setFamilia.add('Maritza');
+console.log(setFamilia);
+
+setFamilia.add('Javascript');
+console.log(setFamilia);
